16cc4d16a575c1ece5008d79c4c836e5d18ca6bc
Hackbright Student <no-reply@hackbrightacademy.com>
2018-02-21 22:31:44 -0800

    added map rendering to modals

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 // Rendering Google Maps //
 
-function initMap() {
+function initMap(data) {
     let sf = {lat: 37.7995971, lng: -122.327749};
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 11,
@@ -18,35 +18,34 @@ function initMap() {
     console.log('I made an info window~')
 
     // doesn't ever make this get request
-    $.get('/dispensaries.json', function(disp_json) {
-        console.log('I am GETting the dispensaries information for map!');
-        console.log(disp_json);
-        if (disp_json.count === 0) {
-            alert('No dispenaries near you offer this strain, sorry!')
-        } else {
-            let marker, disp, html;
+    console.log('I am getting the dispensaries information for map.');
+    if (data.count === 0) {
+        alert('No dispenaries near you offer this strain, sorry!');
+        console.log('no dispensaries')
+    } else {
+        let marker, disp, html;
 
-            for (let key in disp_json.dispensaries) {
-                disp = disp_json.dispensaries[key];
+        for (let key in data.dispensaries) {
+            disp = data.dispensaries[key];
 
-                marker = new google.maps.Marker({
-                  position: new google.maps.LatLng(disp.lat, disp.lng),
-                  map: map,
-                  title: disp.name,
-                });
+            marker = new google.maps.Marker({
+              position: new google.maps.LatLng(disp.lat, disp.lng),
+              map: map,
+              title: disp.name,
+            });
 
-                html = (
-                    '<div class="window-content">' +
-                          '<p><b>Dispensary: </b>' + disp.name + '</p>' +
-                          '<p><b>Address: </b>' + disp.address + '</p>' +
-                          '<a href="https://www.google.com/maps?saddr=My+Location&daddr='+disp.lat+','+disp.lng+'" target="_blank">' +
-                          '<p><b>Take Me There!</b></p></a>' +
-                    '</div>');
+            html = (
+                '<div class="window-content">' +
+                      '<p><b>Dispensary: </b>' + disp.name + '</p>' +
+                      '<p><b>Address: </b>' + disp.address + '</p>' +
+                      '<a href="https://www.google.com/maps?saddr=My+Location&daddr='+disp.lat+','+disp.lng+'" target="_blank">' +
+                      '<p><b>Take Me There!</b></p></a>' +
+                '</div>');
 
-                bindInfoWindow(marker, map, infoWindow, html);
-            } //end for loop
-        } //end else
-    }); //end ajax
+            bindInfoWindow(marker, map, infoWindow, html);
+        } //end for loop
+    }; //end else
+     //end ajax
 
     function bindInfoWindow(marker, map, infoWindow, html) {
         google.maps.event.addListener(marker, 'mouseover', function () {
@@ -57,4 +56,5 @@ function initMap() {
     }
 }
 
-google.maps.event.addDomListener(window, 'load', initMap);
+let modal = document.getElementsByClassName('modal')[0];
+google.maps.event.addDomListener(modal, 'load', initMap);
