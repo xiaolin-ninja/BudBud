ad1f60969a0b816c35992744ccb1432ca7731b44
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-03-08 03:40:39 +0000

    fixing bugs

--- model.py
+++ model.py
@@ -313,7 +313,7 @@ def connect_to_db(app, db_uri="postgresql:///budbud"):
 
     # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
