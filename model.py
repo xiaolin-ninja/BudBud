9324f3286690aea99a2d17e575e876bc5ea09d97
xiaolin-ninja <shirleyxiaolinxu@gmail.com>
2018-03-08 04:15:17 +0000

    more heroku bs

--- model.py
+++ model.py
@@ -320,7 +320,7 @@ def connect_to_db(app, db_uri="postgresql:///budbud"):
     db.init_app(app)
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
